d24e1e42d436cc4715bebc1c0c8aa28cac60e6b6
ketan mehta <meetketanmehta@gmail.com>
2024-04-09 20:12:31 +0530

    tailwind configration

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -1,20 +1,36 @@
-import type { Config } from 'tailwindcss'
+import sharedConfig from '@gymcents/tailwind-config/tailwind.config';
+import type { Config } from 'tailwindcss';
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+const config: Pick<Config, 'presets'> = {
+  presets: [
+    {
+      ...sharedConfig,
+      content: [
+        './app/**/*.{js,ts,jsx,tsx}',
+        './ui/**/*.{js,ts,jsx,tsx}',
+        // h/t to https://www.willliu.com/blog/Why-your-Tailwind-styles-aren-t-working-in-your-Turborepo
+        '../../packages/ui/src/**/*{.js,.ts,.jsx,.tsx}',
+      ],
+      theme: {
+        extend: {
+          ...sharedConfig?.theme?.extend,
+          animation: {
+            ...sharedConfig?.theme?.extend?.animation,
+            // Infinite scroll animation
+            'infinite-scroll': 'infinite-scroll 22s linear infinite',
+          },
+          keyframes: {
+            ...sharedConfig?.theme?.extend?.keyframes,
+            // Infinite scroll animation
+            'infinite-scroll': {
+              '0%': { transform: 'translateX(0)' },
+              '100%': { transform: 'translateX(-150%)' },
+            },
+          },
+        },
       },
     },
-  },
-  plugins: [],
-}
-export default config
+  ],
+};
+
+export default config;
