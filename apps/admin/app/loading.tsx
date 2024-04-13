20af375efc62cb65dd0d730e499113d26230363d
ketan mehta <meetketanmehta@gmail.com>
2024-04-13 11:46:16 +0530

    spinner changed

--- apps/admin/app/loading.tsx
+++ apps/admin/app/loading.tsx
@@ -1,18 +1,1 @@
-"use client";
-import Box from "@mui/material/Box";
-import CircularProgress from "@mui/material/CircularProgress";
-
-export default function Loader() {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-      }}
-    >
-      <CircularProgress size={100} />
-    </Box>
-  );
-}
+export { default } from "@/ui/layout/layout-loader";
